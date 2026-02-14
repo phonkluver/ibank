9f291b7e3a22bf3ddc5c63c9065e5ab3283205c6
Murod <mr.booba@inbox.ru>
2026-02-14 15:26:03 +0500

    Add type payment to pkg/types

---
+++ pkg/ibank/types/types.go
@@ -0,0 +1,30 @@
+package types
+
+type Money int64
+
+// Валюта денег
+type Currency string
+
+// Код валюты
+const (
+	TJS Currency = "TJS"
+	USD Currency = "USD"
+	EUR Currency = "EUR"
+)
+
+type PAN string
+
+type Card struct {
+	id       int
+	PAN      PAN
+	Balance  Money
+	Currency Currency
+	Color    string
+	Name     string
+	Active   bool
+}
+
+type Payment struct {
+	id     int
+	Amount Money
+}
